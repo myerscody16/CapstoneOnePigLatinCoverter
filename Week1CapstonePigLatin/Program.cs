55b006385f4b7c44aee933fbf6e28d46e66d1564
myerscody16 <myerscody16@live.com>
2019-07-19 20:31:20 -0400

    save before leaving dads

--- Week1CapstonePigLatin/Program.cs
+++ Week1CapstonePigLatin/Program.cs
@@ -13,7 +13,7 @@ namespace Week1CapstonePigLatin
                 char[] charArray = word.ToCharArray();
                 if(charArray[0] == 'a' || charArray[0] == 'e' || charArray[0] == 'i' || charArray[0] == 'o' || charArray[0] == 'u')
                 {
-                    Console.WriteLine($"{word}" + "-yay");
+                    Console.WriteLine("\n" + $"{word}" + "-yay");
                 }
                 else
                 {
@@ -33,29 +33,18 @@ namespace Week1CapstonePigLatin
         }
         public static void HandleConsWords(string word)
         {
-            //need to take in a word that starts with a consonant, then finds where a vowel is indexed at, saves the cluster of 
-            //consonants that came before it, puts them, in order, in between "-" and "ay".
             int wordLength = word.Length;
-            Console.WriteLine(wordLength);
             int Avowel = word.IndexOf('a');
             int Evowel = word.IndexOf('e');
             int Ivowel = word.IndexOf('i');
             int Ovowel = word.IndexOf('o');
             int Uvowel = word.IndexOf('u');
-            //causes error because if a word doesnt have all of the vowels, the Xvowel variable returns a -1, making 
-            //the minimum value -1 now, which causes an issue in the range of the clusters below.
-            //**thought, we can make another method that sees if the value of the Xvowel variable is -1, if it is, make it ignored in the min() of the array.
+
             int[] vowelPlaceArray = {Avowel, Evowel, Ivowel, Ovowel, Uvowel};
-
-            //one of the two lines below will make this whole thing work... gotta find which one will work when fixed
-
 
             int[] minValArray = vowelPlaceArray.Where(x => x > 0).ToArray();
             int minVal = minValArray.Min();
-            Console.WriteLine(minVal);
-            //do stuff to make the stuff below work
             string consonantCluster = word.Substring(0, minVal);
-            Console.WriteLine(consonantCluster);
             string letterFollowing1stVowel = word.Substring(minVal, wordLength-minVal);
             Console.WriteLine("\n" + $"{letterFollowing1stVowel}" + $"-{consonantCluster}ay");
 
