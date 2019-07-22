312720de6acd0471b13af355f308b90adde86b69
myerscody16 <myerscody16@live.com>
2019-07-22 10:58:55 -0400

    added loop to program

--- Week1CapstonePigLatin/Program.cs
+++ Week1CapstonePigLatin/Program.cs
@@ -7,20 +7,44 @@ namespace Week1CapstonePigLatin
     {
         static void Main(string[] args)
         {
-            string[] wordArray = SplitSentences("Please input an English word or sentence for conversion to Pig Latin, then press enter.");
-            foreach (string word in wordArray)
+            bool cont = true;
+            while (cont)
             {
-                char[] charArray = word.ToCharArray();
-                if(charArray[0] == 'a' || charArray[0] == 'e' || charArray[0] == 'i' || charArray[0] == 'o' || charArray[0] == 'u')
+                string[] wordArray = SplitSentences("Please input an English word or sentence for conversion to Pig Latin, then press enter.");
+                foreach (string word in wordArray)
                 {
-                    Console.WriteLine("\n" + $"{word}" + "-yay");
+                    char[] charArray = word.ToCharArray();
+                    if (charArray[0] == 'a' || charArray[0] == 'e' || charArray[0] == 'i' || charArray[0] == 'o' || charArray[0] == 'u')
+                    {
+                        Console.WriteLine("\n" + $"{word}" + "-yay");
+                    }
+                    else
+                    {
+                        HandleConsWords(word);
+                    }
                 }
-                else
+                bool cont2 = true;
+                while (cont2)
                 {
-                    HandleConsWords(word);
+                    Console.WriteLine("Would you like to run this program again? (y/n)");
+                    string userInput = Console.ReadLine().ToLower();
+                    if (userInput == "y")
+                    {
+                        cont2 = false;
+                    }
+                    else if(userInput == "n")
+                    {
+                        Console.WriteLine("Goodbye.");
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was not a valid answer, please try again.");
+                        continue;
+                    }
                 }
+                continue;
             }
-            
         }
 
         public static string[] SplitSentences(string message)
